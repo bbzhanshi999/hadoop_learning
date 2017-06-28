eba7c199afaabf24d972bf10b317e583dc21f3ea
bbzhanshi999 <bbzhanshi999@163.com>
2017-06-28 14:59:23 +0800

    Initial commit

---
+++ src/main/java/com/zql/chapter2/MaxTemperature.java
@@ -0,0 +1,42 @@
+package com.zql.chapter2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * hadoop job入口
+ * Created by Administrator on 2017/6/28.
+ */
+public class MaxTemperature {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(MaxTemperature.class);
+        job.setJobName("MaxTemperature");
+        FileInputFormat.addInputPath(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        /*map的outputclass如果与reduce是一致的，就无需下面两条设置*/
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true)?0:1);
+
+    }
+}
