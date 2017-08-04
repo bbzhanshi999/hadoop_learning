5f0f191c68f1d7e1e557d98aee1107b4b21ed10f
bbzhanshi999 <bbzhanshi999@163.com>
2017-08-04 10:44:12 +0800

    chapter1

---
+++ src/main/java/com/zql/chapter1/StreamCompressor.java
@@ -0,0 +1,27 @@
+package com.zql.chapter1;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.IOException;
+
+/**
+ * 测试文件压缩系统
+ * Created by Administrator on 2017/8/4.
+ */
+public class StreamCompressor {
+
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+
+        String compressorClassName = args[0];
+        Class<?> compressorClass = Class.forName(compressorClassName);
+        Configuration conf = new Configuration();
+        CompressionCodec codec = (CompressionCodec)ReflectionUtils.newInstance(compressorClass, conf);
+        CompressionOutputStream out = codec.createOutputStream(System.out);
+        IOUtils.copyBytes(System.in,out,conf);
+        out.finish();
+    }
+}
